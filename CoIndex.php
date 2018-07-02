bc2f887dd7c6a74f1738e906c5fcde26029e1a3c
Zhiyang Xu <sirzesx@outlook.fr>
2018-07-02 23:38:15 +0200

    Ajout de la version CoIndex
    form de la recherche ne fonctionne pas

--- CoIndex.php
+++ CoIndex.php
@@ -7,7 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="bootstrap-4.1.0/dist/css/bootstrap.min.css" rel="stylesheet">
+	
     <link rel="stylesheet" href="css/styleCo.css" />
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="bootstrap-4.1.0/dist/js/bootstrap.min.js"></script>	
+  
+  <!-- Include the plugin's CSS and JS: -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/js/bootstrap-multiselect.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/css/bootstrap-multiselect.css"></script>
+	
+	
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -27,7 +38,7 @@
 			<ul class="navbar-nav">
 
 			  <li class="nav-item">
-				<a class="nav-link" href="connexion.php">Recherche</a>
+				<a id="recherche" class="nav-link"  data-toggle="modal" data-target="#myModal">Recherche</a>
 			  </li>
 
 			</ul>
@@ -35,6 +46,7 @@
 			
 			<ul class="nav navbar-nav ml-auto">
 				<li class="nav-item" ><a class="nav-link" href="">  Message </a></li>
+				<li class="nav-item" ><a class="nav-link" href="">  Liste des Amis </a></li>
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						<img alt="User Pic" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" id="profile-image1" class="img-circle img-responsive">
@@ -49,69 +61,225 @@
 
 		</nav>
 		
+		<div 
+		
 		<div class="container" id="containerInCo">
 			<div class="jumbotron" id="jumbInCo">
+					
+						
 					<div class="row">
-						<div class="col-lg-12">
-						  <h2 class="my-4">Our Team</h2>
+						
+						<div class="card" style="width: 13rem;">
+							<img class="card-img-top" src="img/noImage.png" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title"> #Prenom , #Age </h5>
+								<p class="card-text"> Type de relation : <br/>Ville: <br/>Description: </p>
+								<a href="#" class="btn btn-info">voir profil</a>
+							</div>
 						</div>
-						<div class="col-lg-4 col-sm-6 text-center mb-4">
-						  <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-						  <h3>John Smith
-							<small>Job Title</small>
-						  </h3>
-						  <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+						
+						<div class="card" style="width: 13rem;">
+							<img class="card-img-top" src="img/noImage.png" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title"> #Prenom , #Age </h5>
+								<p class="card-text"> Type de relation : <br/>Ville: <br/>Description: </p>
+								<a href="#" class="btn btn-info">voir profil</a>
+							</div>
 						</div>
-						<div class="col-lg-4 col-sm-6 text-center mb-4">
-						  <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-						  <h3>John Smith
-							<small>Job Title</small>
-						  </h3>
-						  <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+						
+						<div class="card" style="width: 13rem;">
+							<img class="card-img-top" src="img/noImage.png" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title"> #Prenom , #Age </h5>
+								<p class="card-text"> Type de relation : <br/>Ville: <br/>Description: </p>
+								<a href="#" class="btn btn-info">voir profil</a>
+							</div>
 						</div>
-						<div class="col-lg-4 col-sm-6 text-center mb-4">
-						  <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-						  <h3>John Smith
-							<small>Job Title</small>
-						  </h3>
-						  <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+						
+						<div class="card" style="width: 13rem;">
+							<img class="card-img-top" src="img/noImage.png" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title"> #Prenom , #Age </h5>
+								<p class="card-text"> Type de relation : <br/>Ville: <br/>Description: </p>
+								<a href="#" class="btn btn-info">voir profil</a>
+							</div>
 						</div>
-						<div class="col-lg-4 col-sm-6 text-center mb-4">
-						  <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-						  <h3>John Smith
-							<small>Job Title</small>
-						  </h3>
-						  <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+						
+						<div class="card" style="width: 13rem;">
+							<img class="card-img-top" src="img/noImage.png" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title"> #Prenom , #Age </h5>
+								<p class="card-text"> Type de relation : <br/>Ville: <br/>Description: </p>
+								<a href="#" class="btn btn-info">voir profil</a>
+							</div>
 						</div>
-						<div class="col-lg-4 col-sm-6 text-center mb-4">
-						  <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-						  <h3>John Smith
-							<small>Job Title</small>
-						  </h3>
-						  <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+					
+					
+						
+						<div class="card" style="width: 13rem;">
+							<img class="card-img-top" src="img/noImage.png" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title"> #Prenom , #Age </h5>
+								<p class="card-text"> Type de relation : <br/>Ville: <br/>Description: </p>
+								<a href="#" class="btn btn-info">voir profil</a>
+							</div>
 						</div>
-						<div class="col-lg-4 col-sm-6 text-center mb-4">
-						  <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-						  <h3>John Smith
-							<small>Job Title</small>
-						  </h3>
-						  <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+						<div class="card" style="width: 13rem;">
+							<img class="card-img-top" src="img/noImage.png" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title"> #Prenom , #Age </h5>
+								<p class="card-text"> Type de relation : <br/>Ville: <br/>Description: </p>
+								<a href="#" class="btn btn-info">voir profil</a>
+							</div>
 						</div>
-				</div>
-			
+
+						<div class="card" style="width: 13rem;">
+							<img class="card-img-top" src="img/noImage.png" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title"> #Prenom , #Age </h5>
+								<p class="card-text"> Type de relation : <br/>Ville: <br/>Description: </p>
+								<a href="#" class="btn btn-info">voir profil</a>
+							</div>
+						</div>
+
+						<div class="card" style="width: 13rem;">
+							<img class="card-img-top" src="img/noImage.png" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title"> #Prenom , #Age </h5>
+								<p class="card-text"> Type de relation : <br/>Ville: <br/>Description: </p>
+								<a href="#" class="btn btn-info">voir profil</a>
+							</div>
+						</div>
+
+						<div class="card" style="width: 13rem;">
+							<img class="card-img-top" src="img/noImage.png" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title"> #Prenom , #Age </h5>
+								<p class="card-text"> Type de relation : <br/>Ville: <br/>Description: </p>
+								<a href="#" class="btn btn-info">voir profil</a>
+							</div>
+						</div>						
+					</div>
+					
+					<button type="button" id="voirPlus" class="btn btn-primary btn-block"> Voir plus de profils</button>
 			</div>
 		</div>
 		
+	</div>	
 		
+</body>
+	
 		
-	</body>
+
+		<!-- Modal -->
+	<div id="myModal" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">Option de recherche</h4>
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+				</div>
+				<form action="index.php" method="post">
+					<div class="modal-body">
+					
+						<div class="ROption">
+							<label for="sel1">Sexe</label>
+							<select class="form-control" id="sel1">
+								<option value="0" selected="selected">Default</option>
+								<option value="1" >Homme</option>
+								<option value="2" >Femme</option>
+							</select>
+						</div>
+						
+						<div class="ROption">
+							<label for="sel2">Age</label>
+							<select class="form-control" id="sel2">
+								<option value="0" selected="selected">Default</option>
+								<option value="1" >18-25ans</option>
+								<option value="2" >25-35ans</option>
+								<option value="3" >35-50ans</option>
+								<option value="4" >+ de 50ans</option>
+							</select>
+						</div>
+						
+						<div class="ROption">
+							<label for="sel3">Signe Astrologique</label>
+							<select class="form-control" id="sel3">
+								<option value="0" selected="selected">Default</option>
+								<option value="1" >Belier</option>
+								<option value="2" >Taureau</option>
+								<option value="3" >Gesmeaux</option>
+								<option value="4" >Cancer</option>
+								<option value="5" >Lion</option>
+								<option value="6" >Vierge</option>
+								<option value="7" >Balance</option>
+								<option value="8" >Scorpion</option>
+								<option value="9" >Saggittaire</option>
+								<option value="10" >Capricorne</option>
+								<option value="11" >Verseau</option>
+								<option value="12" >Poisson</option>
+								
+							</select>
+						</div>
+						
+						<div class="ROption">
+							<label for="sel4">Type de relations</label>
+							<select class="form-control" id="sel4">
+								<option value="0" selected="selected">Default</option>
+								<option value="1" >Serieux</option>
+								<option value="2" >Rapide</option>
+								<option value="3" >Ami</option>
+							</select>
+						</div>
+						
+						
+						<div class="ROption">
+							<label for="sel5">Centre d'interets</label>
+							<select class="form-control" id="sel5" multiple="multiple">
+								<option value="0" selected="selected">Default</option>
+								<option value="1" >Informatique</option>
+								<option value="2" >Musique</option>
+								<option value="3" >Arts</option>
+								<option value="4" >Sports</option>
+								<option value="5" >Cuisine</option>
+								<option value="6" >Electronique</option>
+								<option value="7" >Jeux Video</option>
+								<option value="8" >Photographie</option>
+								<option value="9" >Couture</option>
+								<option value="10" >Bricolage</option>
+								<option value="11" >Jardinage</option>
+								<option value="12" >Lecture</option>
+								<option value="13" >Serie/Film</option>
+								<option value="14" >Animation Japonaise</option>
+							</select>
+						</div>
+						<div class="modal-footer">
+							<button type="submit" class="btn btn-primary" data-dismiss="modal">Rechercher</button>
+							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						</div>
+					</div>
+					
+					
+					  
+				</form>
+			</div>
+
+		</div>
+	</div>
 	
-	
+
 	
 	<script type="text/javascript" src="js/inscription.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="bootstrap-4.1.0/dist/js/bootstrap.min.js"></script>
-  </body>
+	
+    
+
+ 
+	
+	
+	
+ 
 </html>
 	
 	
